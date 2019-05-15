4b01a3f4e5db0d76d3f55930b851193e1419c84f
thomwolf <thomwolf@gmail.com>
2019-05-15 16:08:48 +0200

    added learned positions

--- model.py
+++ model.py
@@ -3,9 +3,10 @@ from torch.nn import Embedding, MultiheadAttention, LayerNorm, Sequential, Modul
 from torch.nn import functional as F
 
 class SimpleTransformer(nn.Module):
-    def __init__(self, dim, hidden_dim, num_embeddings, num_heads, num_layers, dropout):
+    def __init__(self, dim, hidden_dim, num_embeddings, num_max_positions, num_heads, num_layers, dropout):
         super().__init__()
         self.embeddings = Embedding(num_embeddings, dim)
+        self.position_embeddings = Embedding(num_max_positions, dim)
         self.dropout = Dropout(dropout)
 
         self.attentions = ModuleList()
@@ -19,15 +20,14 @@ class SimpleTransformer(nn.Module):
             self.layer_norms_2.append(LayerNorm(dim, eps=1e-12))
 
     def forward(self, x):
-        token_embeddings = self.embeddings(x)
-        embeddings_output = token_embeddings + self.position_embeddings(positions).expand_as(tensor)
+        positions = torch.arange(x.size(-1), device=x.device).unsqueeze(0)
+        embeddings_output = self.embeddings(x) + self.position_embeddings(positions).expand_as(x)
         embeddings_output = self.dropout(embeddings_output)
 
         for attention, layer_norm_1, feed_forward, layer_norm2 in zip(self.attentions, self.layer_norms_1, self.feed_forwards, self.layer_norms_2):
             attention_output = attention(embeddings_output, attn_mask, need_weights=False)
             attention_output = self.dropout(attention_output)
             middle_output = layer_norm_1(embeddings_output + attention_output)
-
             feed_forward_output = feed_forward(middle_output)
             output = layer_norm2(middle_output + feed_forward_output)
         return output

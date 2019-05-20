e9710776b4f44645cc718e97d4611e054d176bee
thomwolf <thomwolf@gmail.com>
2019-05-20 09:41:30 +0200

    clean up download process

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import torch
 
 from pytorch_pretrained_bert import cached_path
 
-WIKITEXT_2_URL = "https://s3.amazonaws.com/datasets.huggingface.co/wikitext-2.tar.gz"
+WIKITEXT_2_URL = "https://s3.amazonaws.com/datasets.huggingface.co/wikitext-2/"
 
 logger = logging.getLogger(__file__)
 
@@ -21,32 +21,18 @@ def average_distributed_scalar(scalar, args):
     return scalar_t.item()
 
 
-def download_dataset(dataset_path):
-    """ Download and extract wikitext-2 from S3 or local tar.gz file if needed. """
-    try:
-        resolved_dataset_path = cached_path(dataset_path)
-    except EnvironmentError:
-        logger.info("Download dataset from %s", WIKITEXT_2_URL)
-        resolved_dataset_path = cached_path(WIKITEXT_2_URL)
-
-    if os.path.isfile(resolved_dataset_path):
-        logger.info("extracting dataset file %s", resolved_dataset_path)
-        with tarfile.open(resolved_dataset_path, 'r:gz') as archive:
-            archive.extractall()
-    return resolved_dataset_path
-
-
-def get_and_tokenize_dataset(tokenizer, dataset_dir, dataset_cache=None):
+def get_and_tokenize_dataset(tokenizer, dataset_dir=WIKITEXT_2_URL, dataset_cache=None):
     """ Retrieve, tokenize, encode and cache the dataset """
     if dataset_cache and os.path.isfile(dataset_cache):
         logger.info("Load encoded dataset from cache at %s", dataset_cache)
         dataset = torch.load(dataset_cache)
     else:
         logger.info("Download dataset from %s", dataset_dir)
-        dataset_dir = download_dataset(dataset_dir)
         dataset = {}
         for split_name in ['test', 'train', 'valid']:
-            with open(os.path.join(dataset_dir, split_name + '.txt'), "r", encoding="utf-8") as f:
+            full_dataset_path = os.path.join(dataset_dir, split_name + '.txt')
+            dataset_file = cached_path(full_dataset_path)
+            with open(dataset_file, "r", encoding="utf-8") as f:
                 all_lines = f.readlines()
                 dataset[split_name] = ' '.join([line.strip() for line in all_lines if len(line.strip())])
 
@@ -62,4 +48,5 @@ def get_and_tokenize_dataset(tokenizer, dataset_dir, dataset_cache=None):
         if dataset_cache:
             logger.info("Save encoded dataset to cache at %s", dataset_cache)
             torch.save(dataset, dataset_cache)
+
     return dataset
